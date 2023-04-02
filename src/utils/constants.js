1492151913a76db51490431f2a1dd2ba9011b777
Nikita Averyanov <rac1onal@yandex.ru>
2023-04-02 19:31:01 +0300

    refactor: форматировал normalize, удалил папку scripts и лишние js файлы

--- src/utils/constants.js
+++ src/utils/constants.js
@@ -30,10 +30,10 @@ export const initialCards = [
 //*	Конфиг для удобного редактирования класса FormValidator.js	*//
 
 export const validationConfig = {
-	formSelector: '.popup__form',
-	inputSelector: '.popup__input',
-	submitButtonSelector: '.button',
-	inactiveButtonClass: 'button_inactive',
-	inputErrorClass: 'popup__input_type_error',
-	errorClass: 'popup__input-error_visible'
+	formSelector:					'.popup__form',
+	inputSelector:				'.popup__input',
+	submitButtonSelector:	'.button',
+	inactiveButtonClass:	'button_inactive',
+	inputErrorClass:			'popup__input_type_error',
+	errorClass:						'popup__input-error_visible'
 };
